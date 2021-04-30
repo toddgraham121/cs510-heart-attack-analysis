680b988f8dd04c560972bead3483e092b3265a13
Todd Graham <toddgraham121@gmail.com>
2021-04-30 14:02:09 -0700

    Update base.html

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,10 +1,10 @@
+import hello.views
 from django.urls import path, include
 
 from django.contrib import admin
 
 admin.autodiscover()
 
-import hello.views
 
 # To add a new path, first import the app:
 # import blog
